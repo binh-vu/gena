c7369c19f9a43ea2eccb542ea6ab6c6e8fc7967b
Binh Vu <binh@toan2.com>
2022-11-27 11:17:53 -0800

    update ci and test

---
+++ tests/import_test.py
@@ -0,0 +1,18 @@
+import pkgutil
+import gena
+from pathlib import Path
+from importlib import import_module
+
+
+def test_import(pkg=gena, ignore_deprecated: bool = True):
+    stack = [(pkg.__name__, Path(pkg.__file__).parent.absolute())]
+
+    while len(stack) > 0:
+        pkgname, pkgpath = stack.pop()
+        for m in pkgutil.iter_modules([str(pkgpath)]):
+            mname = f"{pkgname}.{m.name}"
+            if ignore_deprecated and mname.find("deprecated") != -1:
+                continue
+            if m.ispkg:
+                stack.append((mname, pkgpath / m.name))
+            import_module(mname)
